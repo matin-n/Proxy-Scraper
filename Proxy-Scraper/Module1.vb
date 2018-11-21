c6de75b95d7301a1f9cfe9d789647a1ba06d76de
Matin <MatinGitHub@gmail.com>
2018-11-21 06:32:35 -0800

    IP2Long
    Added IP Address to Long function
    Applied the IP2Long function on the scraped IP addresses
    Switched list data type from String to Long

--- Proxy-Scraper/Module1.vb
+++ Proxy-Scraper/Module1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Net
 Imports System.Text.RegularExpressions
 
 Module Module1
@@ -9,25 +10,8 @@ Module Module1
         Console.ReadKey()
         Console.WriteLine("Process has started...")
 
-        Dim values As List(Of String) = New List(Of String)
-
-        Try
-            Parallel.ForEach(File.ReadLines(Directory.GetCurrentDirectory() & "\proxysources.txt"),' New ParallelOptions With {.MaxDegreeOfParallelism = 10},
-                             Sub(line)
-                                 Dim value As String = New System.Net.WebClient().DownloadString(line)
-                                 Dim matches As MatchCollection = Regex.Matches(value, "(\d{1,3}\.){3}\d{1,3}") '':(\d+)"
-
-                                 For Each m As Match In matches
-                                     For Each c As Capture In m.Captures
-                                         values.Add(c.Value)
-                                         Console.WriteLine(c.Value & " " & line)
-                                     Next
-                                 Next
-
-                             End Sub)
-        Catch ex As Exception
-            Console.WriteLine(ex)
-        End Try
+        Dim values As List(Of Long) = New List(Of Long)
+        scrapeProxies(values)
 
         Console.WriteLine("Done Scraping...")
         Console.WriteLine("Press any key to list the IP of the scraped proxies...")
@@ -39,5 +23,35 @@ Module Module1
 
         Console.ReadKey()
     End Sub
+
+    Private Sub scrapeProxies(values As List(Of Long))
+        Try
+            Parallel.ForEach(File.ReadLines(Directory.GetCurrentDirectory() & "\proxysources.txt"),' New ParallelOptions With {.MaxDegreeOfParallelism = 10},
+                             Sub(line)
+                                 Dim value As String = New System.Net.WebClient().DownloadString(line)
+                                 Dim matches As MatchCollection = Regex.Matches(value, "(\d{1,3}\.){3}\d{1,3}") '':(\d+)"
+
+                                 For Each m As Match In matches
+                                     For Each c As Capture In m.Captures
+                                         values.Add(IPAddressToLong(IPAddress.Parse(c.Value)))
+                                         Console.WriteLine(c.Value & " " & line)
+                                     Next
+                                 Next
+
+                             End Sub)
+        Catch ex As Exception
+            Console.WriteLine(ex)
+        End Try
+    End Sub
+
+    Public Function IPAddressToLong(address As System.Net.IPAddress) As Long
+        Dim byteIP As Byte() = address.GetAddressBytes()
+
+        Dim ip As Long = CLng(byteIP(3)) << 24
+        ip += CLng(byteIP(2)) << 16
+        ip += CLng(byteIP(1)) << 8
+        ip += CLng(byteIP(0))
+        Return ip
+    End Function
 
 End Module
