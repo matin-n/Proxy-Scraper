a92b2e4b0a58a950686aa1b282a116cc5b1d61c1
Matin <MatinGitHub@gmail.com>
2018-11-21 06:46:16 -0800

    Post JSON data to API
    Added function which utilizes WebRequests to post the proxy IP and current time to the API

--- Proxy-Scraper/Module1.vb
+++ Proxy-Scraper/Module1.vb
@@ -1,8 +1,10 @@
 ﻿Imports System.IO
 Imports System.Net
+Imports System.Text
 Imports System.Text.RegularExpressions
 
 Module Module1
+    Const quote As String = """"
 
     Sub Main()
 
@@ -14,14 +16,13 @@ Module Module1
         scrapeProxies(values)
 
         Console.WriteLine("Done Scraping...")
-        Console.WriteLine("Press any key to list the IP of the scraped proxies...")
+        Console.WriteLine("Press any key to upload the scraped proxy list...")
         Console.ReadKey()
 
-        For Each element In values
-            Console.WriteLine(element)
-        Next
+        uploadProxies(values)
 
-        Console.ReadKey()
+        Console.WriteLine("Done Uploading...")
+        Console.WriteLine("Press any key to close the program")
     End Sub
 
     Private Sub scrapeProxies(values As List(Of Long))
@@ -34,7 +35,7 @@ Module Module1
                                  For Each m As Match In matches
                                      For Each c As Capture In m.Captures
                                          values.Add(IPAddressToLong(IPAddress.Parse(c.Value)))
-                                         Console.WriteLine(c.Value & " " & line)
+                                         'Console.WriteLine(c.Value & " " & line)
                                      Next
                                  Next
 
@@ -42,6 +43,38 @@ Module Module1
         Catch ex As Exception
             Console.WriteLine(ex)
         End Try
+    End Sub
+
+    Private Sub uploadProxies(values As List(Of Long))
+        Dim ProxyAPI_Uri As New Uri("http://proxyapi.tech/php-crud-api-2.0.4/api.php/records/proxyapi/")
+
+        For Each element In values
+            Try
+                Dim postData = Encoding.UTF8.GetBytes("{ " & quote & "ipv4" & quote & ": " & quote & element & quote & ", " & quote & "date_added" & quote & ": " & quote & DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") & quote & " }")
+                Dim result_post = SendJsonPostRequest(ProxyAPI_Uri, postData)
+                Console.WriteLine("{ " & quote & "ipv4" & quote & ": " & quote & element & quote & ", " & quote & "date_added" & quote & ": " & quote & DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") & quote & " }")
+            Catch webExcp As WebException
+                ' If you reach this point, an exception has been caught.
+                Console.WriteLine("A WebException has been caught.")
+                ' Write out the WebException message.
+                'Console.WriteLine(webExcp.ToString())
+                ' Get the WebException status code.
+                Dim status As WebExceptionStatus = webExcp.Status
+                ' If status is WebExceptionStatus.ProtocolError, there has been a protocol error and a WebResponse should exist.
+                ' Display the protocol error.
+                If status = WebExceptionStatus.ProtocolError Then
+                    Console.Write("The server returned protocol error ")
+                    ' Get HttpWebResponse so that you can check the HTTP status code.
+                    Dim httpResponse As HttpWebResponse =
+                       CType(webExcp.Response, HttpWebResponse)
+                    Console.WriteLine(CInt(httpResponse.StatusCode).ToString() &
+                       " - " & httpResponse.StatusCode.ToString())
+                End If
+            Catch ex As Exception
+                ' Code to catch other exceptions
+                Console.WriteLine(ex)
+            End Try
+        Next
     End Sub
 
     Public Function IPAddressToLong(address As System.Net.IPAddress) As Long
@@ -52,6 +85,26 @@ Module Module1
         ip += CLng(byteIP(1)) << 8
         ip += CLng(byteIP(0))
         Return ip
+    End Function
+
+    Public Function SendJsonPostRequest(uri As Uri, jsonDataBytes As Byte()) As String
+        Dim req As WebRequest = WebRequest.Create(uri)
+        req.ContentType = "application/json"
+        req.Method = "POST"
+        req.ContentLength = jsonDataBytes.Length
+
+        Dim stream = req.GetRequestStream()
+        stream.Write(jsonDataBytes, 0, jsonDataBytes.Length)
+        stream.Close()
+
+        Dim response = req.GetResponse().GetResponseStream()
+
+        Dim reader As New StreamReader(response)
+        Dim res = reader.ReadToEnd()
+        reader.Close()
+        response.Close()
+
+        Return res
     End Function
 
 End Module
